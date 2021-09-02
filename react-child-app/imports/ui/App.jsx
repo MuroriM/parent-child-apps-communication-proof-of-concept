1a2a4d37a70e40f89c78cecb1ad8ea109f7086aa
Joao Cardoso <joao.cardoso@yale.edu>
2021-09-02 13:05:01 +0100

    cleaned up debugging code

--- react-child-app/imports/ui/App.jsx
+++ react-child-app/imports/ui/App.jsx
@@ -5,11 +5,7 @@ import { Message } from "./Message";
 import { Form } from "./Form";
 import { Counter } from "./Counter";
 
-// function useForceUpdate() {
-//   const [value, setValue] = useState(0); // integer state
-//   return () => setValue((value) => value + 1); // update the state to force render
-// }
-
+//Global state variables
 store.setState("count", 0); // Create "count" global state
 store.setState("incomingMessageText", "");
 
@@ -18,40 +14,33 @@ export const App = () => {
   const [incomingMessageText, setIncomingMessageText] = useGlobalState(
     "incomingMessageText"
   );
-
   const [count, setCount] = useGlobalState("count");
-
-  const [state, setState] = useState(0);
+  const [state, setState] = useState(0); //will be used to force update
 
   //verify that component has loaded (for debugging purposes)
   useEffect(() => {
     console.log("child loaded, logging from child itself!");
-    // window.removeEventListener("message", API, false);
     window.addEventListener("message", API, false);
-
-    // console.log(window.getEventListners());
-  }); //now this will only run once
+  });
 
   //function that displays message
   function displayMessage(args) {
     //sanity check
-    console.log(`displayMessage function triggered with args ${args}!`);
+    console.log(
+      `displayMessage function triggered on child with args ${args}!`
+    );
 
-    //check messageText is string and different from current
-    if (
-      typeof args.messageText === "string" &&
-      args.messageText != incomingMessageText
-    ) {
+    //check messageText is string
+    if (typeof args.messageText === "string") {
       //check message is actually different before calling this
       setIncomingMessageText(args.messageText);
-      console.log(incomingMessageText);
     }
   }
 
   //function that alerts message
   function alertMessage(args) {
     //sanity check
-    console.log(`alertMessage function triggered with args ${args}!`);
+    console.log(`alertMessage function triggered on child with args ${args}!`);
 
     //check args and alert
     if (typeof args.messageText === "string") {
@@ -61,23 +50,19 @@ export const App = () => {
 
   //function that increments count
   const incrementCount = () => {
-    console.log("here!");
+    console.log("increment function triggered on child!");
     setCount(count + 1);
-    console.log(count);
     console.log(incomingMessageText);
   };
 
   // //function that decrements count
   function decrementCount() {
-    console.log("decrement function triggered!");
+    console.log("decrement function triggered on child!");
     setCount(count - 1);
   }
 
   //API
   const API = (message) => {
-    //record state value upon entering API
-    let original_state = state;
-
     // function adapter
     const functionAdapter = {
       displayMessage,
@@ -90,9 +75,8 @@ export const App = () => {
     if (message.origin === PARENT_URL) {
       //remove event listener if API triggered, as it will be re-initiated on DOM reload
       //this is absolutely necessary or each successive call will create duplicates and crash the app
+      //this also means you must force a re-render at the end of the call handling to re-initiate the event listener
       window.removeEventListener("message", API, false);
-
-      //this also means that each API call must trigger exactly one state change
 
       //sanity check
       console.log("incoming message in child API from parent!");
@@ -102,23 +86,15 @@ export const App = () => {
       functionName = message.data.function;
       args = message.data.args;
 
-      console.log(functionName, args);
-
       //call appropriate function
       if (functionAdapter[functionName]) {
-        if (args) {
-          functionAdapter[functionName](args);
-        } else {
-          functionAdapter[functionName]();
-        }
+        functionAdapter[functionName](args);
       } else {
         throw new Error("invalid function name");
       }
 
       // force re-render
       setState(state + 1);
-
-      // console.log("forcing re-render!");
     } else if (message.origin === CHILD_URL) {
       //sanity check
       console.log("incoming message in child API from own URL");
@@ -134,13 +110,6 @@ export const App = () => {
       <Message />
       <Counter />
       <Form />
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        +
-      </button>
     </div>
   );
 };
